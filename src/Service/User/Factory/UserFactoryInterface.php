c34a9c6d30dce40ac4fcd38687fec608f4569044
adamsky4111 <adamdz411@gmail.com>
2021-03-23 19:56:32 +0100

    add user factory

---
+++ src/Service/User/Factory/UserFactoryInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Service\User\Factory;
+
+use App\Entity\User\User;
+use App\Service\User\Dto\UserDto;
+
+interface UserFactoryInterface
+{
+    public function create(UserDto $dto): User;
+}
