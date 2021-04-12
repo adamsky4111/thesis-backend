5a81e3946d6a5e865d43ee94a2284e3d7876c1aa
adamsky4111 <adamdz411@gmail.com>
2021-04-12 20:44:39 +0200

    use avatar creator in manager and serializer

--- src/Serializer/UserSerializer.php
+++ src/Serializer/UserSerializer.php
@@ -5,7 +5,7 @@ namespace App\Serializer;
 use App\Entity\User\User;
 use App\Serializer\Factory\SerializerFactory;
 use App\Service\User\Dto\UserDto;
-use App\Service\User\Manager\AvatarCreator;
+use App\Service\User\Manager\AvatarCreatorInterface;
 use Symfony\Component\Serializer\Normalizer\ContextAwareDenormalizerInterface;
 use Symfony\Component\Serializer\Normalizer\ContextAwareNormalizerInterface;
 use Symfony\Component\Serializer\Serializer;
@@ -15,11 +15,11 @@ class UserSerializer implements ContextAwareNormalizerInterface, ContextAwareDen
 {
     protected Serializer $serializer;
     protected TranslatorInterface $translator;
-    protected AvatarCreator $avatarCreator;
+    protected AvatarCreatorInterface $avatarCreator;
 
     public function __construct(
         TranslatorInterface $translator,
-        AvatarCreator $avatarCreator,
+        AvatarCreatorInterface $avatarCreator,
     ) {
         $this->serializer = SerializerFactory::getObjectNormalizer();
         $this->translator = $translator;
--- src/Service/User/Manager/UserManagerInterface.php
+++ src/Service/User/Manager/UserManagerInterface.php
@@ -4,6 +4,7 @@ namespace App\Service\User\Manager;
 
 use App\Entity\User\User;
 use App\Service\User\Dto\UserDto;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 interface UserManagerInterface
 {
@@ -16,4 +17,5 @@ interface UserManagerInterface
     public function create(UserDto $dto): User;
     public function update(UserDto $dto, User $user): User;
     public function delete(User $user): void;
+    public function changeAvatar(UploadedFile $file, User $user): string;
 }
