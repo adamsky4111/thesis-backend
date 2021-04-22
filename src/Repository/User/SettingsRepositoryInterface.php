0d8bb403e664be983d6826bd47e0342fcc5d8afd
adamsky4111 <adamdz411@gmail.com>
2021-04-22 16:39:43 +0200

    add settings repository interface, settings manager

---
+++ src/Repository/User/SettingsRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repository\User;
+
+use App\Entity\User\Settings;
+use App\Service\Stream\Filter\FilterInterface;
+use JetBrains\PhpStorm\ArrayShape;
+
+interface SettingsRepositoryInterface
+{
+    public function find(int $id);
+    #[ArrayShape([
+        'items' => "array",
+        'total' => "int",
+        'pages' => "int"
+    ])]
+    public function findWithFilter(FilterInterface $filter): array;
+    public function save(Settings $settings): Settings;
+    public function remove(Settings $settings): Settings;
+}
