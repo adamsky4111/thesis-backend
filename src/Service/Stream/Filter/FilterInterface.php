19e317d5d5a432516fde7a8f111def2d55c5fd22
adamsky4111 <adamdz411@gmail.com>
2021-04-22 16:39:21 +0200

    add settings filter

---
+++ src/Service/Stream/Filter/FilterInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Service\Stream\Filter;
+
+
+interface FilterInterface
+{
+    const TYPE_FROM_TO = 0;
+    const TYPE_PHRASE = 1;
+    const TYPE_BOOLEAN = 2;
+    public function calculateOffset(): int;
+    public function getLike(string $phrase): string;
+    public function isAllowedField(string $field): bool;
+    public function isAllowedSort(string $sort): bool;
+    public function getTypeMapper(): array;
+    public function isPaginated(): bool;
+    public function getPage(): int;
+}
+
---
+++ src/Service/Stream/Filter/SettingsFilter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service\Stream\Filter;
+
+
+final class SettingsFilter extends AbstractFilter implements FilterInterface
+{
+    // fields
+    const FIELD_IS_DEFAULT = 'isDefault';
+    const FIELD_NAME = 'name';
+    const FIELD_AGE = 'ageAllowed';
+
+    // types
+    const TYPE_MAPPER = [
+        self::FIELD_AGE => self::TYPE_FROM_TO,
+        self::FIELD_IS_DEFAULT => self::TYPE_BOOLEAN,
+        self::FIELD_NAME => self::TYPE_PHRASE,
+    ];
+
+    // allowed fields in search
+    const ALLOWED_SEARCH_FIELDS = [
+        self::FIELD_NAME,
+        self::FIELD_IS_DEFAULT,
+        self::FIELD_AGE,
+    ];
+
+    // allowed fields in sort
+    const ALLOWED_SORT_FIELDS = self::ALLOWED_SEARCH_FIELDS;
+
+    public function isAllowedField(string $field): bool
+    {
+        return in_array($field, self::ALLOWED_SEARCH_FIELDS);
+    }
+
+    public function isAllowedSort(string $sort): bool
+    {
+        return in_array($sort, self::ALLOWED_SORT_FIELDS);
+    }
+
+    public function getTypeMapper(): array
+    {
+        return self::TYPE_MAPPER;
+    }
+}
+
